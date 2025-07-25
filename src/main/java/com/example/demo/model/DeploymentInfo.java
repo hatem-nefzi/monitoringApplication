aa1e7f5ce50dcbeb9567403cd3b54552599dd4e9
hatem-nefzi <hatem.nefzibouali@ensi-uma.tn>
2025-07-25 12:27:23 +0100

    fixing deploymentInfo into being null safe

--- src/main/java/com/example/demo/model/DeploymentInfo.java
+++ src/main/java/com/example/demo/model/DeploymentInfo.java
@@ -1,8 +1,5 @@
 package com.example.demo.model;
 
-
-
-
 import io.kubernetes.client.openapi.models.V1Deployment;
 import lombok.Data;
 
@@ -17,10 +14,21 @@ public class DeploymentInfo {
     private Map<String, String> labels;
 
     public DeploymentInfo(V1Deployment deployment) {
-        this.name = deployment.getMetadata().getName();
-        this.namespace = deployment.getMetadata().getNamespace();
-        this.replicas = deployment.getSpec().getReplicas();
-        this.availableReplicas = deployment.getStatus().getAvailableReplicas();
-        this.labels = deployment.getMetadata().getLabels();
+        this.name = deployment.getMetadata() != null ? deployment.getMetadata().getName() : "unknown";
+        this.namespace = deployment.getMetadata() != null ? deployment.getMetadata().getNamespace() : "unknown";
+
+        if (deployment.getSpec() != null && deployment.getSpec().getReplicas() != null) {
+            this.replicas = deployment.getSpec().getReplicas();
+        } else {
+            this.replicas = 0;
+        }
+
+        if (deployment.getStatus() != null && deployment.getStatus().getAvailableReplicas() != null) {
+            this.availableReplicas = deployment.getStatus().getAvailableReplicas();
+        } else {
+            this.availableReplicas = 0;
+        }
+
+        this.labels = deployment.getMetadata() != null ? deployment.getMetadata().getLabels() : null;
     }
 }
